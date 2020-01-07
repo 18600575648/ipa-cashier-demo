6920251aeff4beff72a2693eba76a38f76697b20
18600575648 <liubin>
2020-01-08 06:48:28 +0800

    处理状态同步，返回后查询结果

--- src/main/java/com/ipaloma/jxbpaydemo/MainActivity.java
+++ src/main/java/com/ipaloma/jxbpaydemo/MainActivity.java
@@ -1,35 +1,25 @@
 package com.ipaloma.jxbpaydemo;
 
-import android.Manifest;
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.scheme.SocketFactory;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
@@ -40,8 +30,7 @@ import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.security.KeyStore;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 
 public class MainActivity extends Activity {
     private final static String TAG = "MainActivity";
@@ -55,7 +44,7 @@ public class MainActivity extends Activity {
     private String mResultUrl;
     private RadioGroup mChannel;
     private EditText mBillNumber;
-
+    private PaymentStatusTask mPaymentStatusTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -76,27 +65,29 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 String sandbox = mSandbox.getText().toString().trim();
-                if(sandbox.trim() == "") {
+                if(sandbox.trim().equals("")) {
                     Toast.makeText(mActivity, R.string.no_sandbox_specified, Toast.LENGTH_LONG).show();
                     return;
                 }
-                String billnumber = mSandbox.getText().toString().trim();
-                if(sandbox.trim() == "") {
+                String billnumber = mBillNumber.getText().toString().trim();
+                if(billnumber.trim().equals("")) {
                     Toast.makeText(mActivity, R.string.no_billnumber_specified, Toast.LENGTH_LONG).show();
                     return;
                 }
                 int checkedId = mChannel.getCheckedRadioButtonId();
-                mLoadActivityIntent = getPackageManager().getLaunchIntentForPackage("com.ipaloma.jxbpay");
-                if(mLoadActivityIntent != null) {
-                    mLoadActivityIntent.putExtra("amount", 0.01);				// 支付金额
-                    mLoadActivityIntent.putExtra("sandbox", sandbox);	// 注册商户的二级域名
-                    mLoadActivityIntent.putExtra("title", "经销宝收银台");	// 定义收银台界面的title
-                    mLoadActivityIntent.putExtra("billnumber", billnumber);	// 订单编号
-                    mLoadActivityIntent.putExtra("notifyurl", "http://xxx?orderid="+billnumber);	// 支付完成后，将会调用此url（http post）通知结果(json格式)
-                    mLoadActivityIntent.putExtra("env",  checkedId == R.id.dev ? "dev" : checkedId == R.id.demo ? "demo" : "");
+                //mLoadActivityIntent = getPackageManager().getLaunchIntentForPackage("com.ipaloma.jxbpay");
+                mLoadActivityIntent = new Intent();
+                mLoadActivityIntent.setComponent(new ComponentName("com.ipaloma.jxbpay", "com.ipaloma.jxbpay.MainActivity"));
 
-                    startActivityForResult(mLoadActivityIntent, payment_requestcode);
-                }
+                mLoadActivityIntent.putExtra("amount", 0.01);				// 支付金额
+                mLoadActivityIntent.putExtra("sandbox", sandbox);	// 注册商户的二级域名
+                mLoadActivityIntent.putExtra("title", "经销宝收银台");	// 定义收银台界面的title
+                mLoadActivityIntent.putExtra("billnumber", billnumber);	// 订单编号
+                mLoadActivityIntent.putExtra("notifyurl", "http://xxx?orderid="+billnumber);	// 支付完成后，将会调用此url（http post）通知结果(json格式)
+                mLoadActivityIntent.putExtra("env",  checkedId == R.id.dev ? "dev" : checkedId == R.id.demo ? "demo" : "");
+
+                 startActivityForResult(mLoadActivityIntent, payment_requestcode);
+
                 // TODO the application not installed, try to install it first
             }
         });
@@ -108,43 +99,112 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        Log.d(TAG, "---onActivityResult--- \n" + requestCode + " " + resultCode + " " + (data == null ? "data = null" : data.toString()));
+        Log.d(TAG, "---onActivityResult--- \n" + requestCode + " " + resultCode + " " + (data == null ? "data = null" : data.getExtras().toString()));
 
         if (requestCode == payment_requestcode && data == null)
             return;
         if (requestCode == payment_requestcode && data != null) {
             String message = data.getExtras().getString("message");//得到消息
-            String transctionid = data.getExtras().getString("transctionid");//得到支付交易流水号
-            String resulturl = data.getExtras().getString("resulturl");//得到结果查询url
-            // resultCode : RESULT_OK, RESULT_CANCELED(检查message内容)
-            Toast.makeText(this, resultCode == RESULT_OK ? "支付成功" : "支付失败", Toast.LENGTH_LONG).show();
-            return;
+            String resulturl = data.getExtras().getString("result_url");//得到结果查询url
+
+            Log.d(TAG, "message: " + message + " resulturl : " + (resulturl == null ? "" : resulturl));
+
+            if (BuildConfig.DEBUG)
+                Toast.makeText(this, resultCode == RESULT_OK ? "启动支付成功" : ("启动支付失败\n" + message), Toast.LENGTH_LONG).show();
+
+            // check the status of payment with resulturl
+            if (resulturl != null && !resulturl.trim().equals("")) {
+                if(mPaymentStatusTask != null
+                        && mPaymentStatusTask.getStatus() == AsyncTask.Status.RUNNING  || mPaymentStatusTask.getStatus() == AsyncTask.Status.PENDING)
+                    mPaymentStatusTask.cancel(true);
+
+                mPaymentStatusTask = new PaymentStatusTask(resulturl, 60 * 1000);
+                mPaymentStatusTask.execute();
+            }
         }
     }
 
+    private class PaymentStatusTask extends AsyncTask<Void, Void, String> {
 
-    public static byte[] httpPost(String url, String param) {
-        if (url != null && url.length() != 0) {
-            HttpClient httpClient = createHttpClient();
-            HttpPost httpPost = new HttpPost(url);
+        private final String mUrl;
+        private final long mTimeout;
+        private PaymentStatusTask mInstance;
 
-            try {
-                httpPost.setEntity(new StringEntity(param, "utf-8"));
-                httpPost.setHeader("Content-Type", "application/json;charset=UTF-8");
-                HttpResponse response;
-                if ((response = httpClient.execute(httpPost)).getStatusLine().getStatusCode() != 200) {
-                    Log.e("SDK_Sample.Util", "httpGet fail, status code = " + response.getStatusLine().getStatusCode());
-                    return null;
-                } else {
-                    return EntityUtils.toByteArray(response.getEntity());
+        public PaymentStatusTask(String url, long timeout ){
+            mUrl = url;
+            mInstance = this;
+            mTimeout = timeout;
+        }
+        @Override
+        protected void onPreExecute(){
+            new CountDownTimer(mTimeout, mTimeout) {
+                public void onTick(long millisUntilFinished) {
+                    // You can monitor the progress here as well by changing the onTick() time
                 }
-            } catch (Exception var3) {
-                Log.e("SDK_Sample.Util", "httpPost exception, e = " + var3.getMessage());
-                var3.printStackTrace();
+                public void onFinish() {
+                    // stop async task if not in progress
+                    if (mInstance.getStatus() == AsyncTask.Status.RUNNING  || mInstance.getStatus() == AsyncTask.Status.PENDING) {
+                        mInstance.cancel(true);
+                        // Add any specific task you wish to do as your extended class variable works here as well.
+                    }
+                }
+            }.start();
+        }
+        @Override
+        protected String doInBackground(Void... voids) {
+            JSONObject result = null;
+            boolean  bContinue = true;
+            String content = "";
+
+            while(!isCancelled() && bContinue) {
+                byte[] buf = httpGet(mUrl);
+                if (buf == null || buf.length == 0)
+                    return null;
+                content = new String(buf);
+                Log.d(TAG, content);
+
+                try {
+                    result = new JSONObject(content);
+
+                    if(!result.optString("error","").equals(""))
+                        return content;
+
+                    String status = result.optString("status");
+                    bContinue = status.equals("等待");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Iterator<String> it = result.keys(); it.hasNext(); ) {
+                String keyStr = it.next();
+                Object value = result.opt(keyStr);
+                stringBuilder.append(keyStr + " : " + (value == null ? "" : value.toString()) + "\n");
+            }
+            if (BuildConfig.DEBUG)
+                Toast.makeText(mActivity, stringBuilder.toString(), Toast.LENGTH_LONG).show();
+            return content;
+        }
+    }
+    public static byte[] httpGet(String url) {
+        if (url == null || url.length() == 0)
+            return null;
+
+        HttpClient httpClient = createHttpClient();
+        HttpGet httpGet = new HttpGet(url);
+
+        try {
+            HttpResponse response;
+            if ((response = httpClient.execute(httpGet)).getStatusLine().getStatusCode() != 200) {
+                Log.e(TAG, "httpGet fail, status code = " + response.getStatusLine().getStatusCode());
                 return null;
             }
-        } else {
-            Log.e("SDK_Sample.Util", "httpPost, url is null");
+            return EntityUtils.toByteArray(response.getEntity());
+
+        } catch (Exception e) {
+            Log.e(TAG, "httpGet exception, e = " + e.getMessage());
+            e.printStackTrace();
             return null;
         }
     }
